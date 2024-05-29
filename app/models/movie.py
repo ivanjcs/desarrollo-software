08a0b5aa5c18157745ff47fc1ca1d4fcfbc7c1b0
Cam <barreracamila.an@gmail.com>
2024-05-28 21:11:11 -0300

    clase Movie

---
+++ app/models/movie.py
@@ -0,0 +1,12 @@
+
+class Movie:
+    directors: str
+    year: int
+    start_date: str
+    final_date: str
+    duration: int
+    description: str
+    genre: str
+    classification: str
+    cast: str
+    language: str
