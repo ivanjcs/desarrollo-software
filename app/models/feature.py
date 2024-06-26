6b72c14f68a0c6ce1111a7dea4e417217000465c
ivanjcs <ivanelgallego2@gmail.com>
2024-06-26 11:11:34 -0300

    clase feature arreglada

--- app/models/feature.py
+++ app/models/feature.py
@@ -9,6 +9,6 @@ class Feature(db.Model):
     __tablename__ = 'features'
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date_from: str = Column(DateTime, nullable=False, default=datetime.now())
-    date_to: str = Column(DateTime, nullable=False, deafult=datetime.now()+timedelta(days=2))
+    date_to: str = Column(DateTime, nullable=False, default=datetime.now()+timedelta(weeks=2))
     
     # FALTAN RELACIONES
