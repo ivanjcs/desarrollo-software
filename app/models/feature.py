0652e69923c7b654d2d33ade919006d90720db4e
ivanjcs <ivanelgallego2@gmail.com>
2024-06-26 11:15:28 -0300

    ultimos arreglos

--- app/models/feature.py
+++ app/models/feature.py
@@ -3,9 +3,11 @@ from datetime import datetime,timedelta
 
 from app import db
 from sqlalchemy import Column, DateTime
+from app.models.audit_mixin import AuditMixin
+from app.models.soft_delete import SoftDeleteMixin
 
 @dataclass(init=False,repr=True, eq=True)
-class Feature(db.Model):
+class Feature(SoftDeleteMixin, AuditMixin,db.Model):
     __tablename__ = 'features'
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date_from: str = Column(DateTime, nullable=False, default=datetime.now())
--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,3 +1,7 @@
 from .user_repository import UserRepository
 from .role_repository import RoleRepository
 from .movie_repository import MovieRepository
+from .profile_repository import ProfileRepository
+from .ticket_repository import TicketRepository
+from .room_repository import RoomRepository
+from .feature_repository import FeatureRepository
